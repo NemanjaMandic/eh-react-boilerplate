432c43548058a95f2980db5638e1ec2b850d1882
NemanjaMandic <nemanja.mandic@folkmatic.com>
2019-04-05 00:39:25 +0200

    added husky

--- src/App.js
+++ src/App.js
@@ -12,9 +12,11 @@ class App extends Component {
     const { count } = this.state
 
     const style = count > 10 ? 'warning' : 'disko'
+
     return (
       <div>
         <h1>Ola World!</h1>
+
         <img src="kurac.png" alt="kurac" />
         <h2 className={style}>Count: {this.state.count}</h2>
         <button onClick={() => this.setState({ count: this.state.count + 1 })}>
--- src/App.spec.js
+++ src/App.spec.js
@@ -1,9 +1,13 @@
 import React from 'react'
-import { render, describe, it } from 'react-testing-library'
+import { render } from 'react-testing-library'
 import App from './App'
 
 describe('App', () => {
   it('Renders without error', () => {
     render(<App />)
+  })
+
+  it('Renders without error', () => {
+    expect(true).toBe(true)
   })
 })
